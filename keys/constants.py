006b40f5b44dcfd82da613f0a64465a9ebd76e22
Monjola <mojojobaba@gmail.com>
2024-12-04 05:08:54 +0000

    jg

--- keys/constants.py
+++ keys/constants.py
@@ -2,3 +2,5 @@ TONES = (
     ("Major", "Major"),
     ("Minor", "Minor")
 )
+
+NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
--- keys/models.py
+++ keys/models.py
@@ -4,9 +4,15 @@ from django.conf import settings
 from keys import constants
 
 class Keys(models.Model):
-    name = models.CharField(max_length=8)
-    tonic = models.CharField(max_length=2)
-    tone = models.CharField(blank=True, choices=constants.TONES, max_length=10)
+    name = models.CharField(max_length=8)  # e.g., "C Major"
+    tonic = models.CharField(max_length=2)  # e.g., "C", "D", "E"
+    tone = models.CharField(choices=constants.TONES, max_length=10)  # e.g., "Major", "Minor"
 
     def __str__(self):
         return self.name
+    
+class Scale(models.Model):
+    tone = 
+
+    def __str__(self):
+        return f"{self.key.name} Scale"
