35c036bfab505f0a960f848ab63bad1b32cd2b0c
Dxmaged <mojojobaba@gmail.com>
2025-01-20 00:56:55 +0000

    reset

--- keys/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from . models import Keys, Scale
-# Register your models here.
-admin.site.register(Keys)
-admin.site.register(Scale)
--- keys/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .views import homepage
-
-app_name = "keys"
-
-urlpatterns = [
-    path(""),
-]
